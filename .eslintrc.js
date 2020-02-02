503286664e3c2d43572d1861986d527d450b68bd
Ca Duong <duonghotinhca.97@gmail.com>
2020-02-02 17:08:04 +0200

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true
+      }
+    ],
+    "react/jsx-indent": [2, "tab"],
+    "react/jsx-max-props-per-line": [1, { maximum: 2 }],
+    "react/destructuring-assignment": [1, "always"],
+    "react/jsx-no-duplicate-props": 2
+  }
+};
